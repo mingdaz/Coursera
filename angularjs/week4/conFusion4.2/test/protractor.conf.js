3684623c66766046e619f075f6bce8db4212215a
mingdaz <zhangmd406@gmail.com>
2016-09-08 19:07:14 -0700

    a

---
+++ angularjs/week4/conFusion4.2/test/protractor.conf.js
@@ -0,0 +1,21 @@
+/**
+ * Created by mingdzhang on 8/30/16.
+ */
+exports.config = {
+    allScriptsTimeout: 11000,
+    specs: [
+        'e2e/*.js'
+    ],
+    capabilities: {
+        'browserName': 'chrome'
+    },
+
+    baseUrl: 'http://localhost:3001/',
+
+    framework: 'jasmine',
+    directConnect: true,
+
+    jasmineNodeOpts: {
+        defaultTimeoutInterval: 30000
+    }
+};
