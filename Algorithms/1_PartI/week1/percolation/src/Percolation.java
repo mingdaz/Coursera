ad37a9c8786a6dc079225ede757a506af7206a43
mingdaz <zhangmd406@gmail.com>
2016-12-03 16:33:52 -0800

    reformatting

--- Algorithms/1_PartI/week1/percolation/src/Percolation.java
+++ Algorithms/1_PartI/week1/percolation/src/Percolation.java
@@ -1,116 +1,118 @@
 /**
  * Created by mingdzhang on 12/2/16.
  */
+
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
     // create n-by-n grid, with all sites blocked
     public Percolation(int n) {
-        if(n<1) throw new java.lang.IllegalArgumentException();
+        if (n < 1) throw new java.lang.IllegalArgumentException();
         int i;
         range = n;
-        bottom = n*n+1;
+        bottom = n * n + 1;
         // Create UF private menber
-        UF = new WeightedQuickUnionUF(n*n+2);
+        UF = new WeightedQuickUnionUF(n * n + 2);
         // create block array
-        block = new boolean[n*n];
-        linkBottom = new boolean[n+1];
+        block = new boolean[n * n];
+        linkBottom = new boolean[n + 1];
         //Initial block
-        for(i=0;i<n*n;i++){
-            block[i]=false;
+        for (i = 0; i < n * n; i++) {
+            block[i] = false;
         }
-        for(i=0;i<=n;i++){
-            linkBottom[i]=false;
+        for (i = 0; i <= n; i++) {
+            linkBottom[i] = false;
         }
 
     }
 
     // open site (row, col) if it is not open already
-    public void open(int row, int col){
-        if(notValid(row,col)) throw new java.lang.IndexOutOfBoundsException();
-        if(!isOpen(row,col)){
-            block[ (row-1) * range + (col-1) ]=true;
-            joinNeighbour(row,col);
+    public void open(int row, int col) {
+        if (notValid(row, col)) throw new java.lang.IndexOutOfBoundsException();
+        if (!isOpen(row, col)) {
+            block[(row - 1) * range + (col - 1)] = true;
+            joinNeighbour(row, col);
         }
     }
 
     // is site (row, col) open?
     public boolean isOpen(int row, int col) {
-        if(notValid(row,col)) throw new java.lang.IndexOutOfBoundsException();
-        return block[(row-1)*range+col-1];
+        if (notValid(row, col)) throw new java.lang.IndexOutOfBoundsException();
+        return block[(row - 1) * range + col - 1];
     }
 
     // is site (row, col) full?
     public boolean isFull(int row, int col) {
-        if(notValid(row,col)) throw new java.lang.IndexOutOfBoundsException();
-        if(!isOpen(row,col)) return false;
-        return UF.connected(0,(row-1)*range+col);
+        if (notValid(row, col)) throw new java.lang.IndexOutOfBoundsException();
+        if (!isOpen(row, col)) return false;
+        return UF.connected(0, (row - 1) * range + col);
     }
 
     // does the system percolate?
-    public boolean percolates(){
-        return UF.connected(0,bottom);
+    public boolean percolates() {
+        return UF.connected(0, bottom);
     }
 
-    private boolean notValid(int row,int col){
-        return (row<1||row>range||col<1||col>range);
+    private boolean notValid(int row, int col) {
+        return (row < 1 || row > range || col < 1 || col > range);
     }
 
-    private void joinNeighbour(int row,int col){
-        joinLeft(row,col);
-        joinRight(row,col);
-        joinUp(row,col);
-        joinDown(row,col);
-        checkbottom(row,col);
+    private void joinNeighbour(int row, int col) {
+        joinLeft(row, col);
+        joinRight(row, col);
+        joinUp(row, col);
+        joinDown(row, col);
+        checkbottom(row, col);
     }
 
-    private boolean joinLeft(int row,int col){
-        if(notValid(row,col-1) || !isOpen(row,col-1)) return false;
-        int cur = (row-1)*range+col;
-        UF.union(cur,cur-1);
+    private boolean joinLeft(int row, int col) {
+        if (notValid(row, col - 1) || !isOpen(row, col - 1)) return false;
+        int cur = (row - 1) * range + col;
+        UF.union(cur, cur - 1);
         return true;
     }
 
-    private boolean joinRight(int row,int col){
-        if(notValid(row,col+1) || !isOpen(row,col+1)) return false;
-        int cur = (row-1)*range+col;
-        UF.union(cur,cur+1);
+    private boolean joinRight(int row, int col) {
+        if (notValid(row, col + 1) || !isOpen(row, col + 1)) return false;
+        int cur = (row - 1) * range + col;
+        UF.union(cur, cur + 1);
         return true;
     }
 
-    private boolean joinUp(int row,int col){
-        int cur = (row-1)*range+col;
-        if(row==1) UF.union(0,cur);
-        if(notValid(row-1,col) || !isOpen(row-1,col)) return false;
-        UF.union(cur,cur-range);
+    private boolean joinUp(int row, int col) {
+        int cur = (row - 1) * range + col;
+        if (row == 1) UF.union(0, cur);
+        if (notValid(row - 1, col) || !isOpen(row - 1, col)) return false;
+        UF.union(cur, cur - range);
         return true;
     }
 
-    private boolean joinDown(int row,int col){
-        int cur = (row-1)*range+col;
-        if(notValid(row+1,col) || !isOpen(row+1,col)) return false;
-        UF.union(cur,cur+range);
+    private boolean joinDown(int row, int col) {
+        int cur = (row - 1) * range + col;
+        if (notValid(row + 1, col) || !isOpen(row + 1, col)) return false;
+        UF.union(cur, cur + range);
         return true;
     }
 
-    private void checkbottom(int row,int col){
-        if(!isFull(row,col)) return;
-        int base = range*(range-1);
-        for(int i=1;i<=range;i++){
-            if(isFull(range,i)&&!linkBottom[i]){
-                UF.union(bottom,base+i);
-                linkBottom[i]=true;
+    private void checkbottom(int row, int col) {
+        if (!isFull(row, col)) return;
+        int base = range * (range - 1);
+        for (int i = 1; i <= range; i++) {
+            if (isFull(range, i) && !linkBottom[i]) {
+                UF.union(bottom, base + i);
+                linkBottom[i] = true;
             }
         }
     }
 
     private WeightedQuickUnionUF UF;
-    private boolean [] block;
-    private boolean [] linkBottom;
+    private boolean[] block;
+    private boolean[] linkBottom;
     private int range;
     private int bottom;
+
     // test client (optional)
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
     }
 }
--- Algorithms/1_PartI/week1/percolation/src/PercolationStats.java
+++ Algorithms/1_PartI/week1/percolation/src/PercolationStats.java
@@ -1,26 +1,28 @@
 /**
  * Created by mingdzhang on 12/2/16.
  */
+
 import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.StdOut;
+
 import java.lang.*;
 
 public class PercolationStats {
     // perform trials independent experiments on an n-by-n grid
     public PercolationStats(int n, int trials) {
-        if(n<1||trials<1) throw new IllegalArgumentException();
+        if (n < 1 || trials < 1) throw new IllegalArgumentException();
         double[] x = new double[trials];
-        double tot = n*n;
+        double tot = n * n;
         double open_cnt;
 
-        for(int i=0;i<trials;i++){
+        for (int i = 0; i < trials; i++) {
             Percolation Temp = new Percolation(n);
-            open_cnt=0.0;
-            while(!Temp.percolates()){
-                int row = StdRandom.uniform(1,n+1);
-                int col = StdRandom.uniform(1,n+1);
-                if(!Temp.isOpen(row,col)){
+            open_cnt = 0.0;
+            while (!Temp.percolates()) {
+                int row = StdRandom.uniform(1, n + 1);
+                int col = StdRandom.uniform(1, n + 1);
+                if (!Temp.isOpen(row, col)) {
                     open_cnt++;
                     Temp.open(row, col);
                 }
@@ -30,28 +32,28 @@ public class PercolationStats {
 
         _mean = StdStats.mean(x);
         _stddev = StdStats.stddev(x);
-        double bar = 1.96*_stddev/Math.sqrt(trials);
+        double bar = 1.96 * _stddev / Math.sqrt(trials);
         _confidenceLo = _mean - bar;
         _confidenceHi = _mean + bar;
     }
 
     // sample mean of percolation threshold
-    public double mean(){
+    public double mean() {
         return _mean;
     }
 
     // sample standard deviation of percolation threshold
-    public double stddev(){
+    public double stddev() {
         return _stddev;
     }
 
     // low  endpoint of 95% confidence interval
-    public double confidenceLo(){
+    public double confidenceLo() {
         return _confidenceLo;
     }
 
     // high endpoint of 95% confidence interval
-    public double confidenceHi(){
+    public double confidenceHi() {
         return _confidenceHi;
     }
 
@@ -61,11 +63,11 @@ public class PercolationStats {
     private double _confidenceHi;
 
     // test client (described below)
-    public static void main(String[] args){
-        PercolationStats A = new PercolationStats(Integer.parseInt(args[0]),Integer.parseInt(args[1]));
+    public static void main(String[] args) {
+        PercolationStats A = new PercolationStats(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
 //        PercolationStats A = new PercolationStats(2,100);
-        StdOut.printf("mean                    = %f\n",A.mean());
-        StdOut.printf("stddev                  = %f\n",A.stddev());
-        StdOut.printf("95%% confidence interval = %f, %f\n",A.confidenceLo(),A.confidenceHi());
+        StdOut.printf("mean                    = %f\n", A.mean());
+        StdOut.printf("stddev                  = %f\n", A.stddev());
+        StdOut.printf("95%% confidence interval = %f, %f\n", A.confidenceLo(), A.confidenceHi());
     }
 }
