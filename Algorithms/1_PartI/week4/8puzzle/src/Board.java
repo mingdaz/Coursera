9228e28e37c2293e645bd37f8193ac54ccf8e099
mingdaz <zhangmd406@gmail.com>
2016-12-29 12:28:28 -0800

    assignment

---
+++ Algorithms/1_PartI/week4/8puzzle/src/Board.java
@@ -0,0 +1,122 @@
+import java.util.Stack;
+
+/**
+ * Created by mingdzhang on 12/24/16.
+ */
+public class Board {
+    public Board(int[][] blocks)  {        // construct a board from an n-by-n array of blocks
+        _n = blocks.length;
+        init_board = new int[_n][];
+        int temp_h = 0;
+        int temp_m = 0;
+        int temp_i = 0;
+        int temp_j = 0;
+        int i,j,r,c;
+        for(i = 0; i < _n ; i++){
+            init_board[i] = new int[_n];
+            for(j=0;j<_n;j++){
+                init_board[i][j] = blocks[i][j];
+                if(blocks[i][j]==0){
+                    temp_i = i;
+                    temp_j = j;
+                    continue;
+                }
+                if((i*_n+j+1)!=blocks[i][j]) temp_h++;
+                r = (blocks[i][j]-1)/_n;
+                c = (blocks[i][j]-1)%_n;
+                temp_m = temp_m + Math.abs(r-i) + Math.abs(c-j);
+            }
+        }
+        _hamming = temp_h;
+        _manhattan = temp_m;
+        blank_i = temp_i;
+        blank_j = temp_j;
+    }
+    // (where blocks[i][j] = block in row i, column j)
+    public int dimension() {                 // board dimension n
+        return _n;
+    }
+    public int hamming() {                   // number of blocks out of place
+        return _hamming;
+    }
+    public int manhattan(){ // sum of Manhattan distances between blocks and goal
+        return _manhattan;
+    }
+    public boolean isGoal() {              // is this board the goal board?
+        return _hamming == 0;
+    }
+    public Board twin() {                // a board that is obtained by exchanging any pair of blocks
+        int[][] block = new int[_n][];
+        for(int i= 0; i< _n;i++){
+            block[i] = init_board[i].clone();
+        }
+        if(block[0][0]==0) exchange(0,1,1,0,block);
+        else if(block[0][1]==0) exchange(0,0,1,0,block);
+        else exchange(0,0,0,1,block);
+        return new Board(block);
+    }
+
+    private void exchange(int r1,int c1,int r2, int c2, int[][] block){
+        int temp = block[r1][c1];
+        block[r1][c1] = block[r2][c2];
+        block[r2][c2] = temp;
+    }
+
+    public boolean equals(Object y){// does this board equal y?
+        return this.toString().equals(y.toString());
+    }
+
+    public Iterable<Board> neighbors(){  // all neighboring boards
+        Stack<Board> S = new Stack<>();
+        int[][] block = new int[_n][];
+        for(int i= 0; i< _n;i++){
+            block[i] = init_board[i].clone();
+        }
+        if(blank_i>0){
+            exchange(blank_i,blank_j,blank_i-1,blank_j,block);
+            S.push(new Board(block));
+            exchange(blank_i,blank_j,blank_i-1,blank_j,block);
+        }
+        if(blank_j>0){
+            exchange(blank_i,blank_j,blank_i,blank_j-1,block);
+            S.push(new Board(block));
+            exchange(blank_i,blank_j,blank_i,blank_j-1,block);
+        }
+        if(blank_i<_n-1){
+            exchange(blank_i,blank_j,blank_i+1,blank_j,block);
+            S.push(new Board(block));
+            exchange(blank_i,blank_j,blank_i+1,blank_j,block);
+        }
+        if(blank_j<_n-1){
+            exchange(blank_i,blank_j,blank_i,blank_j+1,block);
+            S.push(new Board(block));
+            exchange(blank_i,blank_j,blank_i,blank_j+1,block);
+        }
+        return S;
+    }
+
+
+    public String toString() {// string representation of this board (in the output format specified below)
+        StringBuilder s = new StringBuilder();
+        s.append(_n + "\n");
+        for (int i = 0; i < _n; i++) {
+            for (int j = 0; j < _n; j++) {
+                s.append(String.format("%2d ", init_board[i][j]));
+            }
+            s.append("\n");
+        }
+        return s.toString();
+    }
+
+    private final int[][] init_board;
+    private final int _n;
+    private final int _hamming;
+    private final int _manhattan;
+    private final int blank_i;
+    private final int blank_j;
+
+    public static void main(String[] args){
+
+
+    } // unit tests (not graded)
+}
---
+++ Algorithms/1_PartI/week4/8puzzle/src/Solver.java
@@ -0,0 +1,179 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Queue;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.MinPQ;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Created by mingdzhang on 12/24/16.
+ */
+public class Solver {
+    public Solver(Board initial){
+        Node GameTree = new Node(initial);
+        Node TwinTree = new Node(initial.twin());
+
+        MinPQ<Node> Q = new MinPQ<Node>(new Comparator<Node>() {
+            @Override
+            public int compare(Node o1, Node o2) {
+                return o1.getPriority() - o2.getPriority();
+            }
+        });
+
+        MinPQ<Node> twinQ = new MinPQ<Node>(new Comparator<Node>() {
+            @Override
+            public int compare(Node o1, Node o2) {
+                return o1.getPriority() - o2.getPriority();
+            }
+        });
+
+        Q.insert(GameTree);
+        twinQ.insert(TwinTree);
+
+        boolean Sol = false;
+        boolean twinSol = false;
+
+        Node temp=null;
+
+        while( !Sol && !twinSol ){
+            // deque first element
+            temp = Q.delMin();
+            // if it is already the goal return
+            if(temp.data.isGoal()){
+                Sol = true;
+                break;
+            }
+
+            for(Board b:temp.data.neighbors()){
+                // if neighbor in Game tree do not add it to Queue
+                if(GameTree.inTree(b)) continue;
+                // otherwise create a new node for it
+                Node child = new Node(b);
+                // set parent pointer
+                child.setParent(temp);
+                // add this node to game tree
+                if(temp.children==null){
+                    temp.children = new ArrayList<>();
+                }
+                temp.children.add(child);
+                // add this node to Q
+                Q.insert(child);
+            }
+
+
+            // do the same for twin Game
+            temp = twinQ.delMin();
+            if(temp.data.isGoal()){
+                twinSol = true;
+                break;
+            }
+
+            for(Board b:temp.data.neighbors()){
+                if(GameTree.inTree(b)) continue;
+                Node child = new Node(b);
+                child.setParent(temp);
+                if(temp.children==null){
+                    temp.children = new ArrayList<>();
+                }
+                temp.children.add(child);
+                twinQ.insert(child);
+            }
+
+        }
+
+        if(Sol){
+            _isSolvable = true;
+            _moves = temp.move;
+            _solution = new Stack<Board>();
+            while(temp!=null){
+                _solution.push(temp.data);
+                temp = temp.parent;
+            }
+        }
+        else{
+            _isSolvable = false;
+            _moves = -1;
+        }
+
+    }           // find a solution to the initial board (using the A* algorithm)
+
+    public boolean isSolvable(){
+        return _isSolvable;
+    }            // is the initial board solvable?
+
+    public int moves(){
+        return _moves;
+    }                     // min number of moves to solve initial board; -1 if unsolvable
+
+    public Iterable<Board> solution(){
+        return _solution;
+    }      // sequence of boards in a shortest solution; null if unsolvable
+
+
+    private class Node {
+        private Node(Board item){
+            data = item;
+            parent = null;
+            children = null;
+            move = 0;
+        }
+        private Board data;
+        private Node parent;
+        private List<Node> children;
+        private int move;
+        private boolean inTree(Board b){
+            if(data.equals(b)) return true;
+            if(children==null||children.size()==0) return false;
+            for(Node n:children){
+                if(n.inTree(b)) return true;
+            }
+            return false;
+        }
+        private void setParent(Node parent){
+            this.parent = parent;
+            setMove(parent.getMove()+1);
+        }
+        private void setMove(int move){
+            this.move = move;
+        }
+        private int getMove(){
+            return move;
+        }
+        private int getPriority(){
+            return move + data.manhattan();
+        }
+    }
+
+
+
+    private boolean _isSolvable;
+    private int _moves;
+    private Stack<Board> _solution;
+
+
+    public static void main(String[] args) {
+
+        // create initial board from file
+        In in = new In(args[0]);
+        int n = in.readInt();
+        int[][] blocks = new int[n][n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                blocks[i][j] = in.readInt();
+        Board initial = new Board(blocks);
+
+        // solve the puzzle
+        Solver solver = new Solver(initial);
+
+        // print solution to standard output
+        if (!solver.isSolvable())
+            StdOut.println("No solution possible");
+        else {
+            StdOut.println("Minimum number of moves = " + solver.moves());
+            for (Board board : solver.solution())
+                StdOut.println(board);
+        }
+    }// solve a slider puzzle (given below)
+}
