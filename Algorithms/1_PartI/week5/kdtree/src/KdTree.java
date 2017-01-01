370148be0cd1667224abed1b4cd0df5e30f1e58a
mingdaz <zhangmd406@gmail.com>
2016-12-31 21:00:42 -0800

    first try on hw5

---
+++ Algorithms/1_PartI/week5/kdtree/src/KdTree.java
@@ -0,0 +1,178 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.SET;
+import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.Stack;
+
+
+/**
+ * Created by mingdzhang on 12/30/16.
+ */
+public class KdTree {
+
+    // construct an empty set of points
+    public KdTree() {
+        size = 0;
+        root = null;
+    }
+
+    // is the set empty?
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    // number of points in the set
+    public int size() {
+        return size;
+    }
+
+    // add the point to the set (if it is not already in the set)
+    public void insert(Point2D p) {
+        if (p == null) throw new NullPointerException("");
+        root = put(root, p, XXX);
+    }
+
+    private Node put(Node x, Point2D p, boolean flag) {
+        if (x == null) {
+            size++;
+            return new Node(p, null, null);
+        }
+        if (!x.p.equals(p)) {
+            if ((flag == XXX && p.x() < x.p.x())
+                    || (flag == YYY && p.y() < x.p.y())) {
+                x.lb = put(x.lb, p, !flag);
+            } else {
+                x.rt = put(x.rt, p, !flag);
+            }
+        }
+        if (x.lb != null || x.rt != null) {
+            double xmin = x.rect.xmin();
+            double ymin = x.rect.ymin();
+            double xmax = x.rect.xmax();
+            double ymax = x.rect.ymax();
+            if(x.lb != null) {
+                xmin = Double.min(xmin, x.lb.rect.xmin());
+                ymin = Double.min(ymin, x.lb.rect.ymin());
+                xmax = Double.max(xmax, x.lb.rect.xmax());
+                ymax = Double.max(ymax, x.lb.rect.ymax());
+            }
+            if(x.rt != null) {
+                xmin = Double.min(xmin, x.rt.rect.xmin());
+                ymin = Double.min(ymin, x.rt.rect.ymin());
+                xmax = Double.max(xmax, x.rt.rect.xmax());
+                ymax = Double.max(ymax, x.rt.rect.ymax());
+            }
+            x.rect = new RectHV(xmin, ymin, xmax, ymax);
+        }
+        return x;
+    }
+    // does the set contain point p?
+    public boolean contains(Point2D p) {
+        if (p == null) throw new NullPointerException("");
+        return contains(root, p, XXX);
+
+    }
+    private boolean contains(Node x, Point2D p, boolean flag) {
+        if (x == null) return false;
+        if (x.p.equals(p)) return true;
+        if (!x.rect.contains(p)) return false;
+        if ((flag == XXX && p.x() < x.p.x())
+                    || (flag == YYY && p.y() < x.p.y())) {
+            return contains(x.lb, p, !flag);
+        } else {
+            return contains(x.rt, p, !flag);
+        }
+    }
+
+    // draw all points to standard draw
+    public void draw() {
+        draw(root, XXX, 0, 1, 0, 1);
+    }
+
+    private void draw(Node x, boolean flag, double xmin, double xmax, double ymin, double ymax) {
+        if (x == null) return;
+        StdDraw.setPenColor(StdDraw.BLACK);
+        StdDraw.setPenRadius(0.01);
+        x.p.draw();
+        StdDraw.setPenRadius();
+        if (flag == XXX) {
+            StdDraw.setPenColor(StdDraw.RED);
+            StdDraw.line(x.p.x(), ymin, x.p.x(), ymax);
+            draw(x.lb, !flag, xmin, x.p.x(), ymin, ymax);
+            draw(x.rt, !flag, x.p.x(), xmax, ymin, ymax);
+        } else {
+            StdDraw.setPenColor(StdDraw.BLUE);
+            StdDraw.line(xmin, x.p.y(), xmax, x.p.y());
+            draw(x.lb, !flag, xmin, xmax, ymin, x.p.y());
+            draw(x.rt, !flag, xmin, xmax, x.p.y(), ymax);
+        }
+    }
+
+    // all points that are inside the rectangle
+    public Iterable<Point2D> range(RectHV rect) {
+        if (rect == null) throw new NullPointerException("");
+        Stack<Point2D> S = new Stack<Point2D>();
+        range(rect, root, S);
+        return S;
+    }
+    private void range(RectHV rect, Node x, Stack<Point2D> S) {
+        if(x == null || !x.rect.intersects(rect)) return;
+        if (rect.contains(x.p)) {
+            S.push(x.p);
+        }
+        range(rect, x.lb, S);
+        range(rect, x.rt, S);
+    }
+
+    // a nearest neighbor in the set to point p; null if the set is empty
+    public Point2D nearest(Point2D p) {
+        if (p == null) throw new NullPointerException("");
+        Double dist = Double.MAX_VALUE;
+        return nearest(p, dist, root);
+    }
+    private Point2D nearest(Point2D p, Double dist, Node x) {
+        if (x == null || x.rect.distanceSquaredTo(p) > dist) return null;
+        double temp = x.p.distanceSquaredTo(p);
+        Point2D res = null;
+        if (temp < dist) {
+            dist = temp;
+            res = x.p;
+        }
+        Point2D lb = nearest(p, dist, x.lb);
+        if (lb != null) {
+            dist = lb.distanceSquaredTo(p);
+            res = lb;
+        }
+        Point2D rt = nearest(p, dist, x.rt);
+        if (rt != null) {
+            dist = rt.distanceSquaredTo(p);
+            res = rt;
+        }
+        return res;
+    }
+
+    private SET<Point2D> pointset;
+
+    private int size;
+    private Node root;
+
+    private static final boolean XXX = true;
+    private static final boolean YYY = false;
+
+    private static class Node {
+        private Point2D p;      // the point
+        private RectHV rect;    // the axis-aligned rectangle corresponding to this node
+        private Node lb;        // the left/bottom subtree
+        private Node rt;        // the right/top subtree
+        public Node(Point2D p, Node l, Node r) {
+            this.p = p;
+            this.lb = l;
+            this.rt = r;
+            this.rect = new RectHV(p.x(),p.y(),p.x(),p.y());
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }                  // unit testing of the methods (optional)
+}
---
+++ Algorithms/1_PartI/week5/kdtree/src/PointSET.java
@@ -0,0 +1,78 @@
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.SET;
+import edu.princeton.cs.algs4.RectHV;
+import edu.princeton.cs.algs4.Stack;
+
+/**
+ * Created by mingdzhang on 12/30/16.
+ */
+public class PointSET {
+
+    // construct an empty set of points
+    public PointSET() {
+        pointset = new SET<>();
+    }
+
+    // is the set empty?
+    public boolean isEmpty() {
+        return pointset.isEmpty();
+    }
+    // number of points in the set
+
+    public int size() {
+        return pointset.size();
+    }
+
+    // add the point to the set (if it is not already in the set)
+    public void insert(Point2D p) {
+        if (p == null) throw new NullPointerException("");
+        pointset.add(p);
+    }
+
+    // does the set contain point p?
+    public boolean contains(Point2D p) {
+        if (p == null) throw new NullPointerException("");
+        return pointset.contains(p);
+    }
+
+    // draw all points to standard draw
+    public void draw() {
+        for (Point2D p:pointset) {
+            p.draw();
+        }
+    }
+
+    // all points that are inside the rectangle
+    public Iterable<Point2D> range(RectHV rect)   {
+        if (rect == null) throw new NullPointerException("");
+        Stack<Point2D> stack = new Stack<Point2D>();
+        for (Point2D p:pointset) {
+            if (rect.contains(p)) {
+                stack.push(p);
+            }
+        }
+        return stack;
+    }
+
+    // a nearest neighbor in the set to point p; null if the set is empty
+    public Point2D nearest(Point2D p) {
+        if (p == null) throw new NullPointerException("");
+        Point2D nearest = null;
+        double distance = Double.MAX_VALUE;
+        for (Point2D point:pointset) {
+            double temp = point.distanceSquaredTo(p);
+            if (temp < distance) {
+                if (temp > 0) {
+                    distance = temp;
+                    nearest = point;
+                }
+            }
+        }
+        return nearest;
+    }
+
+    private SET<Point2D> pointset;
+    public static void main(String[] args) {
+
+    }                  // unit testing of the methods (optional)
+}
